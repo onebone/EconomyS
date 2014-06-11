cb7e8e4fcb157e6c2259ef9716e03067eb6435f1
onebone <yoonchul.jeong00@gmail.com>
2014-06-12 00:28:01 +0900

    Command and event

---
+++ EconomyAPI/src/EconomyAPI/event/bank/ReduceMoneyEvent.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace EconomyAPI\event\bank;
+
+use EconomyAPI\event\EconomyAPIEvent;
+use EconomyAPI\EconomyAPI;
+
+class ReduceMoneyEvent extends EconomyAPIEvent{
+	private $plugin, $username, $amount, $issuer;
+	public static $handlerList;
+	
+	public function __construct(EconomyAPI $api, $username, $amount, $issuer){
+		$this->plugin = $api;
+		$this->username = $username;
+		$this->amount = $amount;
+		$this->issuer = $issuer;
+	}
+	
+	public function getUsername(){
+		return $this->username;
+	}
+	
+	public function getAmount(){
+		return $this->amount;
+	}
+}
