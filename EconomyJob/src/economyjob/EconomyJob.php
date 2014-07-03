3e585fbf7aba51508561c832f4b4f00df75c556c
onebone <yoonchul.jeong00@gmail.com>
2014-07-03 22:46:21 +0900

    Update EconomyJob
    Align codes
    
    List and some functionallity

--- EconomyJob/src/economyjob/EconomyJob.php
+++ EconomyJob/src/economyjob/EconomyJob.php
@@ -10,36 +10,36 @@ use pocketmine\event\Listener;
 use pocketmine\event\block\BlockBreakEvent;
 use pocketmine\event\block\BlockPlaceEvent;
 use pocketmine\utils\TextFormat;
+use pocketmine\Player;
 
 use economyapi\EconomyAPI;
 
 class EconomyJob extends PluginBase implements Listener{
-    /** @var Config */
-    private $jobs;
-    /** @var Config */
-    private $player;
+	/** @var Config */
+	private $jobs;
+	/** @var Config */
+	private $player;
 
 	/** @var  EconomyAPI */
 	private $api;
 
-    /** @var EconomyJob   */
-    private static $instance;
+	/** @var EconomyJob   */
+	private static $instance;
 
-    public function onEnable(){
+	public function onEnable(){
+		@mkdir($this->getDataFolder());
+		if(!is_file($this->getDataFolder()."jobs.yml")){
+			$this->jobs = new Config($this->getDataFolder()."jobs.yml", Config::YAML, yaml_parse($this->readResource("jobs.yml")));
+		}else{
+			$this->jobs = new Config($this->getDataFolder()."jobs.yml", Config::YAML);
+		}
+		$this->player = new Config($this->getDataFolder()."players.yml", Config::YAML);
 
-        @mkdir($this->getDataFolder());
-        if(!is_file($this->getDataFolder()."jobs.yml")){
-            $this->jobs = new Config($this->getDataFolder()."jobs.yml", Config::YAML, yaml_parse($this->readResource("jobs.yml")));
-        }else{
-            $this->job = new Config($this->getDataFolder()."jobs.yml", Config::YAML);
-        }
-        $this->player = new Config($this->getDataFolder()."players.yml", Config::YAML);
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
-
-		$api = EconomyAPI::getInstance();
-        $instance = $this;
-    }
+		$this->api = EconomyAPI::getInstance();
+		self::$instance = $this;
+	}
 
 	private function readResource($res){
 		$path = $this->getFile()."resources/".$res;
@@ -51,85 +51,118 @@ class EconomyJob extends PluginBase implements Listener{
 		return fread($resource, filesize($path));
 	}
 
-    public function onDisable(){
-        $this->player->save();
-    }
+	public function onDisable(){
+		$this->player->save();
+	}
 
-    public function onBlockBreak(BlockBreakEvent $event){
-        $player = $event->getPlayer();
+	public function onBlockBreak(BlockBreakEvent $event){
+		$player = $event->getPlayer();
 		$block = $event->getBlock();
+		
 		$job = $this->jobs->get($this->player->get($player->getName()));
 		if(isset($job[$block->getID().":".$block->getDamage()])){
 			$this->api->addMoney($player, $job[$block->getID().":".$block->getDamage()]);
 		}
-    }
+	}
 
   /*  public function onBlockPlace(BlockPlaceEvent $event){
 
 	}*/
 
-    /**
-     * @return EconomyJob
-    */
-    public static function getInstance(){
-        return static::$instance;
-    }
+	/**
+	 * @return EconomyJob
+	*/
+	public static function getInstance(){
+		return static::$instance;
+	}
 
-    /**
-     * @return Config
-     */
-    public function getJobs(){
-        return $this->jobs;
-    }
+	/**
+	 * @return Config
+	 */
+	public function getJobs(){
+		return $this->jobs;
+	}
 
-    public function onCommand(CommandSender $sender, Command $command, $label, array $params){
-        switch(array_shift($params)){
-            case "join":
-                if($this->player->exists($sender->getName())){
-                    $sender->sendMessage("You already have joined job.");
-                }else{
-                    $job = array_shift($params);
-                    if(trim($job) === ""){
-                        $sender->sendMessage("Usage: /job join <name>");
-                        break;
-                    }
-                    if($this->jobs->exists($job)){
-                        $this->player->set($job);
-                        $sender->sendMessage("You have joined to the job \"$job\"");
-                    }else{
-                        $sender->sendMessage("There's no job named \"$job\"");
-                    }
-                }
-                break;
-            case "retire":
-                if($this->player->exists($sender->getName())){
-                    $this->player->remove($this->player->get($sender->getName()));
-                    $sender->sendMessage("You have retired from the job \"$job\"");
-                }else{
-                    $sender->sendMessage("You don't have job that you've joined");
-                }
-                break;
-            case "list":
-                $output = "Showing job list : \n";
-                foreach($this->jobs as $name => $job){
-                    $info = "";
-                    foreach($job as $id => $money){
-                        $info .= $id." | $".$money."\n";
-                    }
-                    $output .= $name." : ".$info."\n";
-                }
-                $sender->sendMessage($output);
-                break;
-            case "me":
-                if($this->player->exists($sender->getName())){
-                    $sender->sendMessage("Your job : ".$this->player->get($sender->getName()));
-                }else{
-                    $sender->sendMessage("You don't have any jobs you've joined.");
-                }
-                break;
-            default:
-                $sender->sendMessage($command->getUsage());
-        }
-        return true;
-    }
+	public function onCommand(CommandSender $sender, Command $command, $label, array $params){
+		switch(array_shift($params)){
+			case "join":
+				if(!$sender instanceof Player){
+					$sender->sendMessage("Please run this command in-game.");
+				}
+				if($this->player->exists($sender->getName())){
+					$sender->sendMessage("You already have joined job.");
+				}else{
+					$job = array_shift($params);
+					if(trim($job) === ""){
+						$sender->sendMessage("Usage: /job join <name>");
+						break;
+					}
+					if($this->jobs->exists($job)){
+						$this->player->set($sender->getName(), $job);
+						$sender->sendMessage("You have joined to the job \"$job\"");
+					}else{
+						$sender->sendMessage("There's no job named \"$job\"");
+					}
+				}
+				break;
+			case "retire":
+				if(!$sender instanceof Player){
+					$sender->sendMessage("Please run this command in-game.");
+				}
+				if($this->player->exists($sender->getName())){
+					$this->player->remove($this->player->get($sender->getName()));
+					$sender->sendMessage("You have retired from the job \"$job\"");
+				}else{
+					$sender->sendMessage("You don't have job that you've joined");
+				}
+				break;
+			case "list":
+
+				$max = 0;
+				foreach($this->jobs->getAll() as $d){
+					$max += count($d);
+				}
+
+				$max = ceil(($max / 5));
+
+				$page = array_shift($params);
+
+				$page = max(1, $page);
+				$page = min($max, $page);
+				$page = (int)$page;
+
+				$current = 1;
+				$n = 1;
+
+				$output = "Showing job list page $page of $max : \n";
+				foreach($this->jobs->getAll() as $name => $job){
+					$cur = (int)ceil(($n / 5));
+					$info = "";
+					foreach($job as $id => $money){
+						if($cur === $page){
+							$info .= $name." : ".$id." | $".$money."\n";
+						}elseif($cur > $page){
+							break;
+						}
+						++$n;
+					}
+					$output .= $info;
+				}
+				$sender->sendMessage($output);
+				break;
+			case "me":
+				if(!$sender instanceof Player){
+					$sender->sendMessage("Please run this command in-game.");
+				}
+				if($this->player->exists($sender->getName())){
+					$sender->sendMessage("Your job : ".$this->player->get($sender->getName()));
+				}else{
+					$sender->sendMessage("You don't have any jobs you've joined.");
+				}
+				break;
+			default:
+				$sender->sendMessage($command->getUsage());
+		}
+		return true;
+	}
 }
