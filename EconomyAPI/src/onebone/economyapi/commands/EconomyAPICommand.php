fc42f85d818394d577f916495eceb34cfca30f20
onebone <jyc00410@gmail.com>
2015-04-09 20:20:01 +0900

    OMG Great typo

--- EconomyAPI/src/onebone/economyapi/commands/EconomyAPICommand.php
+++ EconomyAPI/src/onebone/economyapi/commands/EconomyAPICommand.php
@@ -8,7 +8,6 @@ use pocketmine\command\PluginIdentifiableCommand;
 use onebone\economyapi\EconomyAPI;
 
 abstract class EconomyAPICommand extends Command implements PluginIdentifiableCommand{
-	private $owingPlugin;
 	
 	public function __construct($name, EconomyAPI $plugin){
 		parent::__construct($name);
