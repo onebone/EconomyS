5abe4f372f2ac683f5eac7fc9171c8c288062ede
onebone <jyc00410@gmail.com>
2015-07-22 23:44:42 +0900

    Fixed money is not sorted correctly, closes #128

--- EconomyAPI/src/onebone/economyapi/task/SortTask.php
+++ EconomyAPI/src/onebone/economyapi/task/SortTask.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace onebone\economyapi\task;
 
@@ -10,11 +10,11 @@ use onebone\economyapi\EconomyAPI;
 
 class SortTask extends AsyncTask{
 	private $player, $moneyData, $addOp, $page, $ops, $banList;
-	
+
 	private $max = 0;
-	
+
 	private $topList = [];
-	
+
 	/**
 	 * @param string	$player
 	 * @param array		$moneyData
@@ -31,28 +31,30 @@ class SortTask extends AsyncTask{
 		$this->ops = $ops;
 		$this->banList = $banList;
 	}
-	
+
 	public function onRun(){
-		arsort($this->moneyData["money"]);
-		
 		$this->topList = $this->getTopList();
 	}
-	
+
 	private function getTopList(){
+		$money = $this->moneyData["money"];
+
+		arsort($money);
+
 		$ret = [];
-		
+
 		$n = 1;
-		$this->max = ceil((count($this->moneyData["money"]) - count($this->banList) - ($this->addOp ? 0 : count($this->ops))) / 5);
+		$this->max = ceil((count($money) - count($this->banList) - ($this->addOp ? 0 : count($this->ops))) / 5);
 		$this->page = (int)min($this->max, max(1, $this->page));
-		
-		foreach($this->moneyData["money"] as $p => $money){
+
+		foreach($money as $p => $money){
 			$p = strtolower($p);
-			
+
 			if(isset($this->banList[$p])) continue;
 			if(isset($this->ops[$p]) and $this->addOp === false) continue;
-			
+
 			$current = (int) ceil($n / 5);
-			
+
 			if($current === $this->page){
 				$ret[$n] = [$p, $money];
 			}elseif($current > $this->page){
@@ -62,18 +64,18 @@ class SortTask extends AsyncTask{
 		}
 		return $ret;
 	}
-	
+
 	public function onCompletion(Server $server){
 		if((($player = $this->player) === "CONSOLE") or ($player = $server->getPlayerExact($this->player)) instanceof Player){
 			$plugin = EconomyAPI::getInstance();
-			
+
 			$output = "- Showing top money list ({$this->page} of {$this->max}) - \n";
 			$message = ($plugin->getMessage("topmoney-format", $this->player, ["%1", "%2", "%3", "%4"])."\n");
-			
+
 			foreach($this->topList as $n => $list){
 				$output .= str_replace(["%1", "%2", "%3"], [$n, $list[0], $list[1]], $message);
 			}
-			
+
 			if($player instanceof Player){
 				$player->sendMessage($output);
 			}else{
