d3b9484f631dc428352b26887e70a47942f79bc0
onebone <jyc00410@gmail.com>
2016-01-01 18:12:54 +0900

    Oops, new file

---
+++ EconomyAPI/src/onebone/economyapi/command/TopMoneyCommand.php
@@ -0,0 +1,64 @@
+<?php
+
+/*
+ * EconomyS, the massive economy plugin with many features for PocketMine-MP
+ * Copyright (C) 2013-2016  onebone <jyc00410@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace onebone\economyapi\command;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+
+use onebone\economyapi\EconomyAPI;
+use onebone\economyapi\task\SortTask;
+
+class TopMoneyCommand extends Command{
+	/** @var EconomyAPI */
+	private $plugin;
+
+	public function __construct(EconomyAPI $plugin){
+		$desc = $plugin->getCommandMessage("topmoney");
+		parent::__construct("topmoney", $desc["description"], $desc["usage"]);
+
+		$this->plugin = $plugin;
+	}
+
+	public function execute(CommandSender $sender, $label, array $params){
+		if(!$this->plugin->isEnabled()) return false;
+		if(!$this->testPermission($sender)) return false;
+
+		$page = (int)array_shift($params);
+
+		$server = $this->plugin->getServer();
+
+		$banned = [];
+		foreach($server->getNameBans()->getEntries() as $entry){
+			if($this->plugin->accountExists($entry->getName())){
+				$banned[] = $entry->getName();
+			}
+		}
+		$ops = [];
+		foreach($server->getOps()->getAll() as $op){
+			if($this->plugin->accountExists($op)){
+				$ops[] = $op;
+			}
+		}
+
+		$task = new SortTask($sender, $this->plugin->getAllMoney(), $this->plugin->getConfig()->get("add-op-at-rank"), $page, $ops, $banned);
+		$server->getScheduler()->scheduleAsyncTask($task);
+	}
+}
