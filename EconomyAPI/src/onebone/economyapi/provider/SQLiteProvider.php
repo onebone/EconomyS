3fd13a1d1c202f1a8f21c08876f4c7690ff1b836
jasonwynn10 <jasonwynn10@gmail.com>
2016-10-05 09:01:12 -0400

    adding SQLite provider too

---
+++ EconomyAPI/src/onebone/economyapi/provider/SQLiteProvider.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ * EconomyS, the massive economy plugin with many features for PocketMine-MP
+ * Copyright (C) 2013-2016  onebone <jyc00410@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+namespace onebone\economyapi\provider;
+
+use onebone\economyapi\EconomyAPI;
+use onebone\economyapi\task\MySQLPingTask;
+use pocketmine\Player;
+
+class SQLiteProvider implements Provider{
+
+}
