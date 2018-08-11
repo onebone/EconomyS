43976c3354b06c3388703e32898d62ec13d9f95e
AmirabasM <amir2013abas13@gmail.com>
2018-08-11 21:54:58 +0430

    Fix Bug

--- EconomyAPI/src/onebone/economyapi/command/GiveMoneyCommand.php
+++ EconomyAPI/src/onebone/economyapi/command/GiveMoneyCommand.php
@@ -20,7 +20,7 @@ class GiveMoneyCommand extends Command{
 	    $this->plugin = $plugin;
     }
 
-    public function execute(CommandSender $sender, string $label, array $args): bool{
+    public function execute(CommandSender $sender, string $label, array $params): bool{
 		if(!$this->plugin->isEnabled()) return false;
 		if(!$this->testPermission($sender)){
 			return false;
