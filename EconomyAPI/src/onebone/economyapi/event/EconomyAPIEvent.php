47bb5d8d5677a3fa9ba53c50f2f080d24e77b2dc
nnnlog <sorisem4106@naver.com>
2019-01-18 20:38:18 +0900

    Use CancellableTrait for new API

--- EconomyAPI/src/onebone/economyapi/event/EconomyAPIEvent.php
+++ EconomyAPI/src/onebone/economyapi/event/EconomyAPIEvent.php
@@ -20,12 +20,15 @@
 
 namespace onebone\economyapi\event;
 
+use pocketmine\event\CancellableTrait;
 use pocketmine\event\plugin\PluginEvent;
 use pocketmine\event\Cancellable;
 
 use onebone\economyapi\EconomyAPI;
 
 class EconomyAPIEvent extends PluginEvent implements Cancellable{
+    use CancellableTrait;
+
 	private $issuer;
 	
 	public function __construct(EconomyAPI $plugin, $issuer){
